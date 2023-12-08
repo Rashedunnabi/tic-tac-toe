3d01a6284e6b8089448b868ac87e0ce2f1c4c57e
Rashedunnabi <mramuhit@gmail.com>
2023-12-09 05:22:51 +0600

    Bootstrapping out App & setting up the Navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import React, { ReactElement } from "react";
-import { Text, AppBootstrap } from "@components";
+import { AppBootstrap } from "@components";
+import Navigator from "@config/navigator";
 
 // SplashScreen.preventAutoHideAsync();
 
 export default function App(): ReactElement {
     return (
         <AppBootstrap>
-            <View style={styles.container}>
-                <Text
-                    onPress={() => {
-                        alert(true);
-                    }}
-                    style={{ fontSize: 25 }}
-                >
-                    {" "}
-                    Bismillah <Text weight="400"> doehrohreo</Text>
-                </Text>
-            </View>
+            <Navigator />
         </AppBootstrap>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
