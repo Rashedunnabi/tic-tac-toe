50c6301622b5612010ba800c1854e3b40427a629
Rashedunnabi <mramuhit@gmail.com>
2023-12-09 01:38:15 +0600

    Google Font

---
+++ src/index.tsx
@@ -0,0 +1,49 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { Game, Home } from "@screens";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        "Delius-Regular": require("../assets/fonts/Delius-Regular.ttf"),
+        "DeliusUnicase-Bold": require("../assets/fonts/DeliusUnicase-Bold.ttf"),
+        "DeliusUnicase-Regular": require("../assets/fonts/DeliusUnicase-Regular.ttf")
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontSize: 35, fontFamily: "Delius-Regular" }}>Delius-Regular</Text>
+            <Text style={{ fontSize: 35, fontFamily: "DeliusUnicase-Bold" }}>
+                DeliusUnicase-Bold
+            </Text>
+            <Text style={{ fontSize: 35, fontFamily: "DeliusUnicase-Regular" }}>
+                DeliusUnicase-Regular
+            </Text>
+            <Text style={{ fontSize: 35 }}>May Allah Bless You.</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
