3ab5cd3c7f78c5362ffadc114a84b33ba85d03fd
Rashedunnabi <mramuhit@gmail.com>
2023-12-10 15:12:30 +0600

    Adding Sounds and Haptics

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,17 +1,11 @@
 import { SafeAreaView } from "react-native";
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState, useRef } from "react";
 import { GradientBackground } from "@components";
 import styles from "./single-player-game.styles";
 import { Board } from "@components";
-import {
-    printFormattedBoard,
-    BoardState,
-    isEmpty,
-    isFull,
-    getAvailableMoves,
-    isTerminal,
-    getBestMove
-} from "@utils";
+import { BoardState, isEmpty, isTerminal, getBestMove } from "@utils";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
 
 export default function Game(): ReactElement {
     // prettier-ignore
@@ -25,6 +19,8 @@ export default function Game(): ReactElement {
     const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
 
     const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+    const popSoundRef = useRef<Audio.Sound | null>(null);
+    const pop2SoundRef = useRef<Audio.Sound | null>(null);
     const gameResult = isTerminal(state);
 
     const insertCell = (cell: number, symbol: "x" | "o"): void => {
@@ -32,6 +28,15 @@ export default function Game(): ReactElement {
         if (stateCopy[cell] || isTerminal(stateCopy)) return;
         stateCopy[cell] = symbol;
         setState(stateCopy);
+
+        try {
+            symbol === "x"
+                ? popSoundRef.current?.replayAsync()
+                : pop2SoundRef.current?.replayAsync();
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     const handleOnCellPressed = (cell: number): void => {
@@ -62,6 +67,26 @@ export default function Game(): ReactElement {
             }
         }
     }, [state, turn]);
+
+    useEffect(() => {
+        //load our sounds
+        const popSoundObject = new Audio.Sound();
+        const pop2SoundObject = new Audio.Sound();
+
+        const loadSounds = async () => {
+            /*eslint-disable @typescript-eslint/no-var-requires */
+            await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
+            popSoundRef.current = popSoundObject;
+            await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
+            pop2SoundRef.current = pop2SoundObject;
+        };
+        loadSounds();
+        return () => {
+            //unload sounds
+            popSoundObject && popSoundObject.unloadAsync();
+            pop2SoundObject && pop2SoundObject.unloadAsync();
+        };
+    }, []);
 
     return (
         <GradientBackground>
