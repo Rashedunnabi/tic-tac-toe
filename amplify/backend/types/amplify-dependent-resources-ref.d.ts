ab1380d9b541458538a51d65bee50f78e110f03f
Rashedunnabi <mramuhit@gmail.com>
2024-01-03 00:27:43 +0600

    AWS amplify configure

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "tictactoe": {
+    "ticTacToe": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "tictactoeb2c59e72": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   }
 }
