ad4681ac7139f521c8d4ea126dec0f392b3573b4
Rashedunnabi <mramuhit@gmail.com>
2024-01-03 11:56:35 +0600

    faul kaz

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React, { ReactElement } from "react";
 import { AppBootstrap } from "@components";
 import Navigator from "@config/navigator";
 import { SettingsProvider } from "@contexts/settings-context";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.Amplify.configure(config);
 
 export default function App(): ReactElement {
     return (
